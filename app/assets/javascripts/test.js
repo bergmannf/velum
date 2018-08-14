e3865905f2a2ab26321b9f943ddb7a652b1d4694
Florian Bergmann <fbergmann@suse.de>
2018-08-14 16:30:52 +0200

    Fix bsc#1097175: Move endsWith to polyfills.
    It is also used by the automation to bootstrap the cluster.
    
    (cherry picked from commit dd101f5bf84e89dcbb7bc1dd5063c335a075eb80)

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,12 +1,3 @@
 // Include here scripts that are only required for test env (e.g.: polyfills)
 
 $.fx.off = true;
-
-if (!String.prototype.endsWith) {
-	  String.prototype.endsWith = function(search, this_len) {
-		    if (this_len === undefined || this_len > this.length) {
-			      this_len = this.length;
-		    }
-		    return this.substring(this_len - search.length, this_len) === search;
-	  };
-}
